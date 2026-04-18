c12c63c312205a18d9859ba10b4cea81b8b7b0e4
Carolina Damacena Peneda <carolpeneda@ufmg.br>
2026-04-18 17:32:37 -0300

    feat: finaliza api de gerenciamento de tarefas

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,12 @@
-// src/index.ts
-import express from 'express';
+import express from "express";
+import { tarefaRoutes } from "./routes/tarefa.routes";
 
 const app = express();
 const PORTA = 3333;
 
-// Middleware "tradutor" de JSON (CRUCIAL)
-app.use(express.json()); 
+app.use(express.json());
 
-// --- NOSSAS ROTAS ---
-
-// Rota 1: GET na raiz
-app.get('/', (req, res) => {
-  // Não usamos o 'req' aqui
-  
-  // Usamos o 'res' para enviar uma resposta
-  res.status(200).json({ mensagem: 'API funcionando!' });
-});
-
-// Rota 2: POST na raiz
-app.post('/', (req, res) => {
-  // Não usamos o 'req'
-  
-  res.status(201).send('Recebemos seu POST! Obrigado!');
-});
-
-// --- FIM DAS ROTAS ---
+app.use('/tarefas', tarefaRoutes);
 
 app.listen(PORTA, () => {
   console.log(`🚀 Servidor rodando na porta ${PORTA}`);
